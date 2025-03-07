428948cb0965193d49cc7796fb8c35eb855043c0
Pooja  Hirani <pooja.hirani93@gmail.com>
2025-03-07 22:05:06 +0000

    Moves the snake in the current direction by adding a new block in front and removing the tail unless it's growing

--- snake.py
+++ snake.py
@@ -6,7 +6,15 @@ class Snake:
         self.body = [(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)]
         self.direction = pygame.Vector2(0, -1)
         self.grow = False
-        
+
     def draw(self, screen):
         for block in self.body:
             pygame.draw.rect(screen, GREEN, (block[0], block[1], BLOCK_SIZE, BLOCK_SIZE))
+    
+    def move(self):
+        if not self.grow:
+            tail = self.body.pop(0)  # Remove the last block unless growing
+        head = self.body[-1]
+        new_head = (head[0] + self.direction.x * BLOCK_SIZE, head[1] + self.direction.y * BLOCK_SIZE)
+        self.body.append(new_head)
+        self.grow = False        
