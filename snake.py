1eb0c26b3efc5cdb77bae893a74aff3b8276664d
Pooja  Hirani <pooja.hirani93@gmail.com>
2025-03-07 22:04:16 +0000

    Draws each block of the snake's body on the screen

--- snake.py
+++ snake.py
@@ -6,3 +6,7 @@ class Snake:
         self.body = [(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)]
         self.direction = pygame.Vector2(0, -1)
         self.grow = False
+        
+    def draw(self, screen):
+        for block in self.body:
+            pygame.draw.rect(screen, GREEN, (block[0], block[1], BLOCK_SIZE, BLOCK_SIZE))
